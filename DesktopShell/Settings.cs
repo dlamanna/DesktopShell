2f0bdff0b0e9d218587fc716f6ab3de1bcdc9e54
dlamanna <dustin.lamanna@gmail.com>
2019-09-17 23:09:06 -0700

    Refined the settings reading code

--- DesktopShell/Settings.cs
+++ DesktopShell/Settings.cs
@@ -17,18 +17,42 @@ namespace DesktopShell.Properties {
         public static string showsDirectory = "";
         public static string moviesDirectory = "";
         public static string musicDirectory = "";
+        public static string videoPlayer = "";
         public static bool hourlyChimeChecked = false;
         public static List<bool> multiscreenEnabled;
         public static bool checkVersion = false;
-        /**/public static System.Drawing.Point positionSave;
+        public static Regex fontRegex;
+        public static Regex backgroundRegex;
+        public static Regex hourlyChimeRegex;
+        public static Regex screensRegex;
+        public static Regex updateRegex;
+        public static Regex musicRegex;
+        public static Regex gamesRegex;
+        public static Regex moviesRegex;
+        public static Regex showsRegex;
+        public static Regex videoPlayerRegex;
+        public static Regex positionRegex;
+        
+        /**/
+        public static System.Drawing.Point positionSave;
         
         public Settings() {
             // // To add event handlers for saving and changing settings, uncomment the lines below:
             //
             this.SettingChanging += this.SettingChangingEventHandler;
-            //
             this.SettingsSaving += this.SettingsSavingEventHandler;
-            //
+
+            fontRegex = new Regex("fontColor");
+            backgroundRegex = new Regex("backgroundColor");
+            hourlyChimeRegex = new Regex("hourlyChime");
+            screensRegex = new Regex("screensEnabled");
+            updateRegex = new Regex("updateCheck");
+            musicRegex = new Regex("musicDirectory");
+            gamesRegex = new Regex("gamesDirectory");
+            moviesRegex = new Regex("moviesDirectory");
+            showsRegex = new Regex("showsDirectory");
+            videoPlayerRegex = new Regex("videoPlayer");
+            positionRegex = new Regex("positionSave");
         }
         
         private void SettingChangingEventHandler(object sender, System.Configuration.SettingChangingEventArgs e) {
@@ -39,42 +63,75 @@ namespace DesktopShell.Properties {
             // Add code to handle the SettingsSaving event here.
         }
 
-        public static void scanSettings()
-        {
+        public static void scanSettings() {
             multiscreenEnabled = new List<bool>();
             //change stuff here to read settings into program
-            using (var sr = new StreamReader("settings.ini"))
+            try
             {
-                foregroundColor = System.Drawing.ColorTranslator.FromHtml(scanLine(sr));
-                backgroundColor = System.Drawing.ColorTranslator.FromHtml(scanLine(sr));
-                hourlyChimeChecked = System.Convert.ToBoolean(scanLine(sr));
-                string[] screenStrings = scanLine(sr).Split(',');
-                for (int i = 0; i < screenStrings.Length; i++)
+                using (var sr = new StreamReader("settings.ini"))
                 {
-                    //GlobalVar.log("!!! Screen" + (i+1) + " enabled: " + screenStrings[i]);
-                    multiscreenEnabled.Add(System.Convert.ToBoolean(screenStrings[i]));
+                    while (!sr.EndOfStream)
+                    {
+                        string rawLine = sr.ReadLine();
+                        string curLine = scanLine(rawLine);
+                        if (fontRegex.IsMatch(rawLine))
+                            foregroundColor = System.Drawing.ColorTranslator.FromHtml(curLine);
+                        else if (backgroundRegex.IsMatch(rawLine))
+                            backgroundColor = System.Drawing.ColorTranslator.FromHtml(curLine);
+                        else if (hourlyChimeRegex.IsMatch(rawLine))
+                            hourlyChimeChecked = System.Convert.ToBoolean(curLine);
+                        else if (screensRegex.IsMatch(rawLine))
+                        {
+                            string[] screenStrings = curLine.Split(',');
+                            for (int i = 0; i < screenStrings.Length; i++)
+                            {
+                                //GlobalVar.log("!!! Screen" + (i+1) + " enabled: " + screenStrings[i]);
+                                multiscreenEnabled.Add(System.Convert.ToBoolean(screenStrings[i]));
+                            }
+                        }
+                        else if (updateRegex.IsMatch(rawLine))
+                            checkVersion = System.Convert.ToBoolean(curLine);
+                        else if (musicRegex.IsMatch(rawLine))
+                            musicDirectory = curLine;
+                        else if (gamesRegex.IsMatch(rawLine))
+                            gamesDirectory = curLine;
+                        else if (moviesRegex.IsMatch(rawLine))
+                            moviesDirectory = curLine;
+                        else if (showsRegex.IsMatch(rawLine))
+                            showsDirectory = curLine;
+                        else if (videoPlayerRegex.IsMatch(rawLine))
+                            videoPlayer = curLine;
+                        else if (positionRegex.IsMatch(rawLine))
+                        {
+                            var positionString = curLine;
+                            var g = Regex.Replace(positionString, @"[\{\}a-zA-Z=]", "").Split(',');
+                            positionSave = new System.Drawing.Point(int.Parse(g[0]), int.Parse(g[1]));
+                        }
+                    }
+                    writeSettings(false);
                 }
-
-                checkVersion = System.Convert.ToBoolean(scanLine(sr));
-                musicDirectory = scanLine(sr);
-                gamesDirectory = scanLine(sr);
-                moviesDirectory = scanLine(sr);
-                showsDirectory = scanLine(sr);
-                
-                /**/var positionString = scanLine(sr);
-                var g = Regex.Replace(positionString, @"[\{\}a-zA-Z=]", "").Split(',');
-                positionSave = new System.Drawing.Point(int.Parse (g[0]),int.Parse( g[1]));
-            }    
+            }
+            catch (IOException e)
+            {
+                GlobalVar.log(e.Message);
+            }
         }
-        public static string scanLine(StreamReader sr) 
-        {
+        public static string scanLine(StreamReader sr) {
             string tempLine = "";
             int i = ((tempLine = sr.ReadLine()).IndexOf("=") + 1);
             return tempLine.Substring(i);
         }
-        public static void writeSettings()
+        public static string scanLine(string line)
         {
-            string[] tempLines = new string[10];
+            string tempLine = "";
+            int i = ((tempLine = line).IndexOf("=") + 1);
+            return tempLine.Substring(i);
+        }
+        public static void writeSettings() {
+            writeSettings(true);
+        }
+        public static void writeSettings(bool toFile) {
+            string[] tempLines = new string[11];
             tempLines[0] = "fontColor=" + System.Drawing.ColorTranslator.ToHtml(foregroundColor);
             tempLines[1] = "backgroundColor=" + System.Drawing.ColorTranslator.ToHtml(backgroundColor);
             tempLines[2] = "hourlyChime=" + hourlyChimeChecked.ToString();
@@ -84,9 +141,15 @@ namespace DesktopShell.Properties {
             tempLines[6] = "gamesDirectory=" + gamesDirectory;
             tempLines[7] = "moviesDirectory=" + moviesDirectory;
             tempLines[8] = "showsDirectory=" + showsDirectory;
-            /**/tempLines[9] = "positionSave=" + positionSave.X + "," + positionSave.Y;
+            tempLines[9] = "videoPlayer=" + videoPlayer;
+            tempLines[10] = "positionSave=" + positionSave.X + "," + positionSave.Y;
 
-            foreach (string s in tempLines) File.WriteAllLines("settings.ini", tempLines);
+            if (toFile) {
+                foreach (string s in tempLines) File.WriteAllLines("settings.ini", tempLines);
+            }
+            else {
+                foreach (string s in tempLines) GlobalVar.log(s);
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,15 +1,24 @@
-DesktopShell
+## DesktopShell
+###### <em>Created in Visual Studio 2017</em>
 ============
-
-Created in Visual Studio 2017
 
 This program serves as a replacement of desktop shortcuts and batch files, and an extension to the Windows start menu.
 
+____
 
+This is mostly meant as a code example since many of the features of this program launch other programs I coded which are not bundled in this repository. If you want to try it anyways, add your [regular expression](https://regexr.com/), program executable path, and commandline arguments in separate lines in the shortcuts.txt file. Add one line spacing between statements, and if no commandline arguments are required, put a dash on the third line.
 
-This is mostly meant as a code example since many of the features of this program launch other programs I coded which are not bundled in this repository. If you want to try it anyways, add your regular expression, program executable path, and arguments in separate lines in the shortcuts.txt file.
+***
 
-Example:
-(^txt$){1}
-C:\notepad.exe
-"C:\Program Files (x86)\testfile.txt"
+### Example:
+
+<p>
+(^txt$){1}<br>
+C:\notepad.exe<br>
+"C:\Program Files (x86)\testfile.txt"<br>
+C:\notepad.exe<br>
+"C:\Program Files (x86)\testfile2.txt"<br>
+<br>
+(^img$){1}<br>
+C:\paint.exe<br>
+-</p>
